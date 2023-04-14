2cd032a230f023dacc51ffe5197a744903b984ed
Leonardo Viana <leonardoviana00@hotmail.com>
2023-04-14 17:05:39 -0300

    feat(filme): detalhes do filme

--- filmes/src/pages/Filme/index.js
+++ filmes/src/pages/Filme/index.js
@@ -1,8 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
 
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "a2c6a8c2b609082ed866a195b31c8f9c",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("FILME NÃO ENCONTRADO");
+            })
+        }
+
+        loadFilme();
+
+        return() => {
+            console.log("componente desmontado")
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        );
+    }
+
+
     return(
-        <div>
-            <h1>Bem-vindo à página detalhes do filme!</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/> 
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10 </strong>
         </div>
     );
 }
