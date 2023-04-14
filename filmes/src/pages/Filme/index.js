7188880733e65a5fd09958d88c08fc3ae996c8a2
Leonardo Viana <leonardoviana00@hotmail.com>
2023-04-14 17:33:45 -0300

    feat(filme): acao botao trailer

--- filmes/src/pages/Filme/index.js
+++ filmes/src/pages/Filme/index.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 import './filme-info.css'
 
 function Filme(){
     const { id } = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -22,6 +23,8 @@ function Filme(){
             })
             .catch(()=>{
                 console.log("FILME NÃO ENCONTRADO");
+                navigate("/", {replace: true});
+                return;
             })
         }
 
@@ -30,7 +33,7 @@ function Filme(){
         return() => {
             console.log("componente desmontado")
         }
-    }, [])
+    }, [ navigate, id])
 
     if(loading){
         return(
@@ -53,7 +56,7 @@ function Filme(){
             <div className='area-buttons'>
                 <button>Salvar</button>
                 <button>
-                    <a href="#">
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
